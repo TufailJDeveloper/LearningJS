8cea2c9b4451f7ef56a0e6564e115fc9a4322593
Tufail Ahmad Telwani <wanitufail012@gmail.com>
2026-01-17 20:38:09 +0000

    Learn basic functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,14 +1,32 @@
 'use strict'
 
-let hasDrivingLiceance = false;
-const hasPassed = true;
+// let hasDrivingLiceance = false;
+// const hasPassed = true;
 
-if(hasPassed){
-    hasDrivingLiceance = true; 
-}
+// if(hasPassed){
+//     hasDrivingLiceance = true; 
+// }
 
-if(hasDrivingLiceance) {
-    console.log("Passed the test");
-}
+// if(hasDrivingLiceance) {
+//     console.log("Passed the test");
+// }
 
-const interface = "abcd"
+// const interface = "abcd"
+
+// ------------------- functions--------------------
+
+
+
+ function foods(apples, mangoes){
+
+    console.log(apples,mangoes)
+
+    const juice=`i ate ${apples} apples and ${mangoes} mangoes`
+
+    return juice;
+ }
+
+
+
+ const result=foods()
+ console.log(result)
