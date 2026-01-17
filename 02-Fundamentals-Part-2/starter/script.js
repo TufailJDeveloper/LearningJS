59ddc138b48e2e4256b6e0887ebcf88feca43def
Tufail Ahmad Telwani <wanitufail012@gmail.com>
2026-01-17 21:56:04 +0000

    Functions calling another function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -48,12 +48,12 @@
 
 //  //Function Expression
 
-     const calculatedAge2=function (birthYear){
+//      const calculatedAge2=function (birthYear){
 
-     const calculatedAge =2026 -birthYear;
+//      const calculatedAge =2026 -birthYear;
 
-     return calculatedAge;
-}
+//      return calculatedAge;
+// }
 
 // console.log(calculatedAge2(2001))
 // cookieStore
@@ -68,15 +68,42 @@
 
 ///Arrow Functions
 
-const calAge3 = birthyear => 2026 - birthyear;
+// const calAge3 = birthyear => 2026 - birthyear;
  
 
-console.log(calAge3(2001));
+// console.log(calAge3(2001));
 
-const retiresIN = (birthyear, name) => {
-    const age = 2026 - birthyear;
-    const retiresIn = `${name} retires in ${65 - age}`
-    return retiresIn;
-}
+// const retiresIN = (birthyear, name) => {
+//     const age = calAge3(birthyear);
+//     const retiresIn = `${name} retires in ${65 - age}`
+//     return retiresIn;
+// }
 
-console.log(retiresIN(2001, "tufail"))
+// console.log(retiresIN(2001, "tufail"))
+
+
+
+
+
+
+////Function calling another function
+
+  function cutFruits(fruit){
+
+        return fruit * 3;
+  }
+
+
+ function foods(apples, mangoes){
+
+    const applePieces=cutFruits(apples);
+    const mangoPieces=cutFruits(mangoes);
+
+
+    const juice=`i got ${applePieces} pieces apples and ${mangoPieces} pieces mangoes`
+
+    return juice;
+ }
+
+
+ console.log(foods(2,5))
