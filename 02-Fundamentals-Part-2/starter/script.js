a855d9bc513434d853e2d5dce3b70cd1572aab3e
Tufail Ahmad Telwani <wanitufail012@gmail.com>
2026-01-17 19:49:34 +0000

    activate the strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let hasDrivingLiceance = false;
+const hasPassed = true;
+
+if(hasPassed){
+    hasDrivingLiceance = true; 
+}
+
+if(hasDrivingLiceance) {
+    console.log("Passed the test");
+}
+
+const interface = "abcd"
