f8db4f29e4182b7128e8f7ee5a69c4616884693c
Tufail Ahmad Telwani <wanitufail012@gmail.com>
2026-01-17 21:08:30 +0000

    learn function declarations and function expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -17,16 +17,47 @@
 
 
 
- function foods(apples, mangoes){
+//  function foods(apples, mangoes){
 
-    console.log(apples,mangoes)
+//     console.log(apples,mangoes)
 
-    const juice=`i ate ${apples} apples and ${mangoes} mangoes`
+//     const juice=`i ate ${apples} apples and ${mangoes} mangoes`
 
-    return juice;
- }
+//     return juice;
+//  }
 
 
 
- const result=foods()
- console.log(result)
+//  const result=foods()
+//  console.log(result)
+
+
+
+///function expressions and declarations
+
+
+///Function Declaration
+function calculateAge(birthYear){
+
+     const calculatedAge = 2026 -birthYear;
+
+     return calculatedAge;
+}
+
+
+
+ //Function Expression
+
+     const calculatedAge2=function (birthYear){
+
+     const calculatedAge =2026 -birthYear;
+
+     return calculatedAge;
+}
+
+
+
+
+   const returnedAge = calculateAge(1997)
+
+    console.log(returnedAge)
