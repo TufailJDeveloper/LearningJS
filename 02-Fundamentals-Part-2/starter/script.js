361c3246de55fd8e1c1591143f127e93afde117c
Tufail Ahmad Telwani <wanitufail012@gmail.com>
2026-01-20 12:35:59 +0000

    Challenge 3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -362,6 +362,54 @@ console.log(userObject.getSummary())
 
 
 
+///CHALLENGE 3:
+
+const mark={
+    
+    fullName :'Mark Miller',
+    mass : 78,
+    height: 1.69,
+    
+    calcBMI :function(){
+        this.bmi= this.mass / (this.height * this.height)
+        return this.bmi;
+    }
+}
+
+const john={
+    
+    fullName :'John Smith',
+    mass : 92,
+    height: 1.95,
+    
+    calcBMI :function(){
+        this.bmi= this.mass / (this.height * this.height)
+        return this.bmi;
+    }
+}
+
+console.log(mark.calcBMI())
+
+console.log(john.calcBMI())
+
+if(mark.calcBMI() > john.calcBMI()){
+    
+    console.log(`${mark.fullName} BMI (${mark.calcBMI()}) is higher than ${ john.fullName }(${john.calcBMI()})!`)
+}
+ 
+   else if(john.calcBMI() > mark.calcBMI()){
+    
+    console.log(`${john.fullName} BMI (${john.calcBMI()}) is higher than ${ mark.fullName }(${mark.calcBMI()})!`)
+}
+    
+
+  
+
+
+
+
+
+
 
 
 
