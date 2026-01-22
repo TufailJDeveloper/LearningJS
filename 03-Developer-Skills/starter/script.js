87d789c90f443e49aa2ad82831325f0209a7aa95
Tufail Ahmad Telwani <wanitufail012@gmail.com>
2026-01-22 16:26:34 +0000

    Debugging with the console and Breakpoints

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -5,36 +5,72 @@
 
 ///Calculate temperature Amplitude
 
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 
-const calcTempAmplitude= function(temp){
+// const calcTempAmplitude= function(temp){
 
-    let biggest=temp[0]
-    let smallest=temp[0]
+//     let biggest=temp[0]
+//     let smallest=temp[0]
 
-    for(let i=1; i<temp.length; i++){
+//     for(let i=1; i<temp.length; i++){
 
-        const curTemp=temp[i];
+//         const curTemp=temp[i];
 
-        if(typeof curTemp !== 'number') continue;
+//         if(typeof curTemp !== 'number') continue;
 
-        if(curTemp > biggest){
-            biggest=curTemp
-        }
+//         if(curTemp > biggest){
+//             biggest=curTemp
+//         }
 
 
-         if(curTemp < smallest){
-            smallest=curTemp
-        }
-    
+//          if(curTemp < smallest){
+//             smallest=curTemp
+//         }
+
+//     }
+
+//     console.log(biggest, smallest)
+//     return biggest-smallest;
+// }
+
+// const amplitude= calcTempAmplitude(temperatures)
+// console.log(amplitude)
+
+
+
+
+//Debugging with the console and Breakpoints
+
+const measureKelvin=function(){
+    const measurement ={
+        type :'temp',
+        unit:'celsius',
+
+
+        //C) FIX
+        value:Number(prompt('Degree celcius:'))
     }
 
-    console.log(biggest, smallest)
-    return biggest-smallest;
+    // B) FIND
+    console.table(measurement)
+
+    console.log(measurement.value)
+    // console.warn(measurement.value)
+    // console.error(measurement.value)
+    const kelvin=measurement.value + 273;
+    return kelvin;
 }
 
-const amplitude= calcTempAmplitude(temperatures)
-console.log(amplitude)
+//A) IDENTIFY THE BUG
+
+console.log(measureKelvin())
+
+
+
+
+
+
+
 
 
