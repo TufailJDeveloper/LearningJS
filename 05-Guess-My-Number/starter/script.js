dc5b89a46325c72ec1c46e79d01e97f597c66d1e
Tufail Ahmad Telwani <wanitufail012@gmail.com>
2026-01-26 13:08:22 +0000

    HANDLING CLICK EVENTS

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,21 +3,40 @@
 
 ///Selecting and Manipulating Elements
 
-console.log(document.querySelector('.message').textContent); 
+// console.log(document.querySelector('.message').textContent); 
 
-document.querySelector('.message').textContent ='Correct Number!'
+// document.querySelector('.message').textContent ='Correct Number!'
 
-console.log(document.querySelector('.message').textContent); 
+// console.log(document.querySelector('.message').textContent); 
 
 
 
-document.querySelector('.number').textContent = 13
+// document.querySelector('.number').textContent = 13
 
-document.querySelector('.score').textContent =10
+// document.querySelector('.score').textContent =10
 
-document.querySelector('.guess').value=23
+// document.querySelector('.guess').value=23
 
-console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.guess').value);
+
+
+
+///HANDLING CLICK EVENTS
+
+document.querySelector('.check').addEventListener('click' , function(){
+
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent='No number!'
+    }
+}
+
+)
+
+
+
 
 
 
