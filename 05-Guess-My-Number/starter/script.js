10599f6a9730c3cec0877994e13cd221eb995429
Tufail Ahmad Telwani <wanitufail012@gmail.com>
2026-01-26 14:14:35 +0000

    IMPLEMENTING THE GAME LOGIC

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -23,17 +23,68 @@
 
 ///HANDLING CLICK EVENTS
 
+
+
+///IMPLEMENTING THE GAME LOGIC
+const secretNumber = Math.trunc(Math.random() *20) +1;
+
+let score=20;
+document.querySelector('.number').textContent=secretNumber;
+
 document.querySelector('.check').addEventListener('click' , function(){
 
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if(!guess){
-        document.querySelector('.message').textContent='No number!'
+        document.querySelector('.message').textContent='No secretNumber!'
     }
+
+    if(guess === secretNumber){
+
+        document.querySelector('.message').textContent ='Correct Number!'
+
+    }
+
+    else if(guess > secretNumber){
+        if(score>1){
+
+      
+        document.querySelector('.message').textContent='Too High!'
+        score--;
+        document.querySelector('.score').textContent=score
+
+          }
+
+          else{
+            
+        document.querySelector('.message').textContent='You Lost the game!'
+          }
+      
+        }
+   
+      else if(guess < secretNumber){
+        if(score>1){
+
+        document.querySelector('.message').textContent='Too Low!'
+        score--;
+        document.querySelector('.score').textContent=score
+
+        
+    }
+     else{
+            
+        document.querySelector('.message').textContent='You Lost the game!'
+          }
+
 }
 
-)
+
+
+
+})
+
+
 
 
 
