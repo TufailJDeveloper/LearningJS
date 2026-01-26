e3265631a8096306665730aca9032f7e4636f591
Tufail Ahmad Telwani <wanitufail012@gmail.com>
2026-01-26 14:38:25 +0000

    Manipulating CSS Styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -23,9 +23,11 @@
 
 ///HANDLING CLICK EVENTS
 
-
-
 ///IMPLEMENTING THE GAME LOGIC
+
+
+///Manipulating CSS Styles
+
 const secretNumber = Math.trunc(Math.random() *20) +1;
 
 let score=20;
@@ -36,16 +38,24 @@ document.querySelector('.check').addEventListener('click' , function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
+
+    /// When there is no input
     if(!guess){
         document.querySelector('.message').textContent='No secretNumber!'
     }
 
+
+    /// When Player Wins
     if(guess === secretNumber){
 
         document.querySelector('.message').textContent ='Correct Number!'
 
-    }
+        document.querySelector('body').style.backgroundColor= '#60b347'
 
+        document.querySelector('number').style.width='30rem'
+    }
+    
+    /// When guess is too high
     else if(guess > secretNumber){
         if(score>1){
 
@@ -63,6 +73,7 @@ document.querySelector('.check').addEventListener('click' , function(){
       
         }
    
+       /// When guess is too low 
       else if(guess < secretNumber){
         if(score>1){
 
